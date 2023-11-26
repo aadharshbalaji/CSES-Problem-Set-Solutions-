c5ce8c38991f9e635440e7ac3e304153d3968734
Aadharsh R <102973336+aadharshbalaji@users.noreply.github.com>
2023-11-27 03:57:16 +0530

    2nd week files

---
+++ Sorting and Searching/1. Distinct Numbers.py
@@ -0,0 +1,12 @@
+import sys
+#sys.stdin,sys.stdout = open("input.txt",'r'),open("output.txt",'w')
+
+def solve():
+    n = int(input())
+    arr = set(input().split())
+    print(len(arr))
+
+
+def main():
+    solve()
+main()
